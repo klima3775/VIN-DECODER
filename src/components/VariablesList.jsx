a81e7978c6847502619a5ead58cae53c730fe902
klima3775 <113065590+klima3775@users.noreply.github.com>
2024-09-13 00:41:08 +0300

    Refactor import statements in VariablesList component

--- src/components/VariablesList.jsx
+++ src/components/VariablesList.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { fetchVariablesList } from "../api/loadVariables"; // Импортируем функцию
-
+import { fetchVariablesList } from "../api/loadVariables";
 const VariablesList = () => {
   const [variables, setVariables] = useState([]);
   const [error, setError] = useState("");
